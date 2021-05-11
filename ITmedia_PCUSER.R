56eb0caa9d26b21362e2b1698f868777f5c86fb2
unknown <59862637+suzuna@users.noreply.github.com>
2021-05-12 01:56:58 +0900

    ITmedia_PCUSERの一覧ページとランキングページの取得を実装

---
+++ ITmedia_PCUSER.R
@@ -0,0 +1,22 @@
+# yymm:その月のトップページなら""（もしくはyymm）、あるいはyymm
+get_ITmedia_PCUSER_articlelist <- function(yymm,UA){
+  if (yymm=="") {
+    url <- "https://www.itmedia.co.jp/pcuser/subtop/archive/"
+  } else {
+    url <- str_c("https://www.itmedia.co.jp/pcuser/subtop/archive/",yymm,".html")
+  }
+  
+  page <- session(url,user_agent(UA)) %>% 
+    read_html()
+  res <- read_ITmedia_articlelist_html(page,"ITmedia PCUSER")
+  return(res)
+}
+
+get_ITmedia_PCUSER_ranking <- function(UA){
+  url <- "https://www.itmedia.co.jp/pcuser/subtop/ranking/"
+  
+  page <- session(url,user_agent(UA)) %>% 
+    read_html()
+  res <- read_ITmedia_ranking_html(page,"ITmedia PCUSER")
+  return(res)
+}
