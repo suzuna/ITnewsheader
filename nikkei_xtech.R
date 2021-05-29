3e295cfe1b3c41c6f00c889dcc0225795cd1422d
unknown <59862637+suzuna@users.noreply.github.com>
2021-05-30 03:57:01 +0900

    日経xtechの一覧ページとランキングページの取得を実装

---
+++ nikkei_xtech.R
@@ -0,0 +1,60 @@
+get_nikkei_xtech_articlelist <- function(p,UA){
+  url <- str_glue("https://xtech.nikkei.com/top/latest.html?bn=news&M=50&P={p}")
+  
+  page <- session(url,user_agent(UA)) %>% 
+    read_html()
+  tmp <- page %>% 
+    html_elements("article.l-main_primary") %>% 
+    html_elements("li.p-articleList_item>a.p-articleList_item_link")
+  
+  title <- tmp %>% 
+    html_elements("div.p-articleList_item_text>h3.p-articleList_item_title") %>% 
+    html_text(trim=TRUE)
+  url <- tmp %>% 
+    html_attr("href") %>% 
+    str_c("https://xtech.nikkei.com",.)
+  subtitle <- tmp %>% 
+    html_elements("div.p-articleList_item_text>p.p-articleList_item_subTitle") %>% 
+    html_text(trim=TRUE)
+  date <- tmp %>% 
+    html_elements("div.p-articleList_item_text>time.p-articleList_item_date") %>% 
+    html_text(trim=TRUE) %>% 
+    as.Date(format="%Y.%m.%d")
+  res <- data.frame(
+    source="日経クロステック",
+    date=date,
+    subtitle=subtitle,
+    title=title,
+    url=url
+  )
+  return(res)
+}
+
+get_nikkei_xtech_ranking <- function(UA){
+  url <- "https://xtech.nikkei.com/ranking/"
+  
+  page <- session(url,user_agent(UA)) %>% 
+    read_html()
+  tmp <- page %>% 
+    html_elements("div#categoryTabToday>ul.p-articleRankList>li.p-articleRankList_item")
+  
+  title <- tmp %>% 
+    html_elements("div.p-articleRankList_item_text>h3.p-articleRankList_item_title") %>% 
+    html_text(trim=TRUE)
+  url <- tmp %>% 
+    html_elements("a") %>% 
+    html_attr("href") %>% 
+    str_c("https://xtech.nikkei.com",.)
+  subtitle <- tmp %>% 
+    html_elements("div.p-articleRankList_item_text>div.p-articleRankList_item_subTitle") %>% 
+    html_text(trim=TRUE)
+  date <- NA
+  res <- data.frame(
+    source="日経クロステック",
+    date=date,
+    subtitle=subtitle,
+    title=title,
+    url=url
+  )
+  return(res)
+}
