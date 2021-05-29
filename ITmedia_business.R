0a72c5cd2acfa7ff683625904417b319f9c59e5e
unknown <59862637+suzuna@users.noreply.github.com>
2021-05-30 04:17:49 +0900

    ITmedia_businessの一覧ページとランキングページの取得を実装

---
+++ ITmedia_business.R
@@ -0,0 +1,22 @@
+# yymm:その月のトップページなら""（もしくはyymm）、あるいはyymm
+get_ITmedia_business_articlelist <- function(yymm,UA){
+  if (yymm=="") {
+    url <- "https://www.itmedia.co.jp/business/subtop/archive/"
+  } else {
+    url <- str_c("https://www.itmedia.co.jp/business/subtop/archive/",yymm,".html")
+  }
+  
+  page <- session(url,user_agent(UA)) %>% 
+    read_html()
+  res <- read_ITmedia_articlelist_html(page,"ITmedia ビジネス")
+  return(res)
+}
+
+get_ITmedia_business_ranking <- function(UA){
+  url <- "https://www.itmedia.co.jp/business/subtop/ranking/"
+  
+  page <- session(url,user_agent(UA)) %>% 
+    read_html()
+  res <- read_ITmedia_ranking_html(page,"ITmedia ビジネス")
+  return(res)
+}
