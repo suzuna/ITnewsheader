0c2a8c8d8d433b1c998dcdea1fa48843e8f5577e
unknown <59862637+suzuna@users.noreply.github.com>
2021-05-31 01:44:38 +0900

    sleep_timeを追加

--- ITmedia_business.R
+++ ITmedia_business.R
@@ -1,5 +1,5 @@
 # yymm:その月のトップページなら""（もしくはyymm）、あるいはyymm
-get_ITmedia_business_articlelist <- function(yymm,UA){
+get_ITmedia_business_articlelist <- function(yymm,UA,sleep_time){
   if (yymm=="") {
     url <- "https://www.itmedia.co.jp/business/subtop/archive/"
   } else {
@@ -9,14 +9,16 @@ get_ITmedia_business_articlelist <- function(yymm,UA){
   page <- session(url,user_agent(UA)) %>% 
     read_html(encoding="Shift-JIS")
   res <- read_ITmedia_articlelist_html(page,"ITmedia ビジネス")
+  Sys.sleep(sleep_time)
   return(res)
 }
 
-get_ITmedia_business_ranking <- function(UA){
+get_ITmedia_business_ranking <- function(UA,sleep_time){
   url <- "https://www.itmedia.co.jp/business/subtop/ranking/"
   
   page <- session(url,user_agent(UA)) %>% 
     read_html(encoding="Shift-JIS")
   res <- read_ITmedia_ranking_html(page,"ITmedia ビジネス")
+  Sys.sleep(sleep_time)
   return(res)
 }
